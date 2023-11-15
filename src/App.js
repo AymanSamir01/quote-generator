43c25733f6bd8da6376ba47c984c41e32bb8f37d
AymanSamir01 <oa1827166@gmail.com>
2023-11-15 16:15:24 +0200

    add social share icon

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import {
+  WhatsappIcon,
+  WhatsappShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+} from "react-share";
+import React, { useState } from "react";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +40,42 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
         </div>
+        <FacebookShareButton
+          url={`https://api.quotable.io/quotes/${quote._id}`}
+          quote={quote.content}
+          hashtag={"#" + quote.author}
+          title={quote.author + " once said: " + quote.content}
+        >
+          <FacebookIcon size={32} round={true} />
+        </FacebookShareButton>
+        <WhatsappShareButton
+          url={`https://api.quotable.io/quotes/${quote._id}`}
+          quote={quote.content}
+          hashtag={"#" + quote.author}
+          title={quote.author + " once said: " + quote.content}
+        >
+          <WhatsappIcon
+            size={32}
+            round={true}
+            style={{ marginInline: "15px" }}
+          />
+        </WhatsappShareButton>
+        <TwitterShareButton
+          url={`https://api.quotable.io/quotes/${quote._id}`}
+          quote={quote.content}
+          hashtag={"#" + quote.author}
+          title={quote.author + " once said: " + quote.content}
+        >
+          <TwitterIcon size={32} round={true} />
+        </TwitterShareButton>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
